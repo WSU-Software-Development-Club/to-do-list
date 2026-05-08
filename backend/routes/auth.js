b7a863e67e15f1e53b02406cce6bbdb74686b062
Reid Pace <rwpace06@gmail.com>
2026-05-07 17:33:07 -0700

    signup dir

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup } = require("../services/auth");
+const { signup } = require("../auth/signup");
 
 router.post("/signup", signup);
 
